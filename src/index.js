e8da35f6ac06fc9c431f8174f133c785c313a5fc
Jacob Bare <jacob@limit0.io>
2018-11-08 09:57:05 -0600

    Add graceful shutdown

--- src/index.js
+++ src/index.js
@@ -9,8 +9,23 @@ const run = async () => {
   log(`Starting '${pkg.name}'...`);
   const mongoose = await db;
   log(`MongoDB connected to ${mongoose.client.s.url}`);
-  await express(PORT, HOSTNAME);
+  const server = await express(PORT, HOSTNAME);
   log(`> Ready on on http://${HOSTNAME}:${PORT}`);
+
+  const graceful = () => {
+    log(`Stopping '${pkg.name}'...`);
+    server.close(() => {
+      log('Web server stopped.');
+      mongoose.close(false, () => {
+        log('MongoDB connection closed.');
+        log('> Stopped');
+        process.exit();
+      });
+    });
+  };
+
+  process.on('SIGTERM', graceful);
+  process.on('SIGINT', graceful);
 };
 
 run().catch(e => setImmediate(() => { throw e; }));
