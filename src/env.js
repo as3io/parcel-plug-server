ed4b377d58fbcfa84a2de79527297877c3fbece7
Jacob Bare <jacob@limit0.io>
2018-11-08 09:36:48 -0600

    Add HOSTNAME and MONGO_DB env vars

--- src/env.js
+++ src/env.js
@@ -1,5 +1,28 @@
-const { port, cleanEnv } = require('envalid');
+const { isURL } = require('validator');
+const {
+  cleanEnv,
+  makeValidator,
+  port,
+} = require('envalid');
+
+const mongodsn = makeValidator((v) => {
+  const opts = { protocols: ['mongodb'], require_tld: false, require_protocol: true };
+  if (isURL(v, opts)) return v;
+  throw new Error('Expected a Mongo DSN string with mongodb://');
+});
+
+const nonemptystr = makeValidator((v) => {
+  const err = new Error('Expected a non-empty string');
+  if (v === undefined || v === null || v === '') {
+    throw err;
+  }
+  const trimmed = String(v).trim();
+  if (!trimmed) throw err;
+  return trimmed;
+});
 
 module.exports = cleanEnv(process.env, {
+  MONGO_DSN: mongodsn({ desc: 'The MongoDB DSN to connect to.' }),
+  HOSTNAME: nonemptystr({ desc: 'The Express web server host name.', default: 'localhost' }),
   PORT: port({ desc: 'The Express web server port.', default: 2218 }),
 });
